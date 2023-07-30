d8c265953e6ff472b0a84a028ae4386639a362b6
silvanshade <silvanshade@users.noreply.github.com>
2023-07-30 09:53:33 -0600

    Expose `::less_than` to silence unused warning

--- crates/cxx-swift/src/ffi/swift/ast/import_path/module.rs
+++ crates/cxx-swift/src/ffi/swift/ast/import_path/module.rs
@@ -1,3 +1,12 @@
+use crate::abi::swift::ast::import_path::module;
+
 pub(crate) mod builder;
 
 pub use crate::abi::swift::ast::import_path::module::Module;
+
+impl<'ctx> Module<'ctx> {
+    #[inline]
+    pub fn less_than(&self, other: &Module<'ctx>) -> bool {
+        module::ffi::cxx_operator_less_than(self, other)
+    }
+}
